acf648c395b7f2fd014427df18c5c3db590a7c9f
oz115 <yjhg1105@naver.com>
2024-03-19 04:01:01 +0900

    1주차

---
+++ oz/1주차/popup.js
@@ -0,0 +1,12 @@
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.scripting.executeScript({
+    target: { tabId: tabs[0].id },
+    files: ["script.js"],
+  });
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.type === "calculationResult") {
+    document.getElementById("average").textContent = message.avg;
+  }
+});
---
+++ oz/1주차/script.js
@@ -0,0 +1,82 @@
+// 특정 텍스트를 포함하는 엘리먼트 찾기
+function findElementByText(selector, text) {
+  let elements = document.querySelectorAll(selector);
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].innerText.includes(text)) {
+      return elements[i];
+    }
+  }
+  return null;
+}
+
+//학점으로 변환
+function getScores(grades) {
+  const gradeMap = {
+    "A+": 4.5,
+    A0: 4.0,
+    "B+": 3.5,
+    B0: 3.0,
+    "C+": 2.5,
+    C0: 2.0,
+    "D+": 1.5,
+    D0: 1.0,
+    F: 0.0,
+    P: null,
+    NP: null,
+  };
+
+  const gradetoscore = [];
+
+  grades.forEach((grade) => {
+    gradetoscore.push(gradeMap[grade]);
+  });
+
+  return gradetoscore;
+}
+
+// "2024학년도 1" 찾기
+var specificElement = findElementByText('th[colspan="9"]', "2023학년도 2");
+
+// 특정 엘리먼트의 부모인 테이블을 찾기
+var table = specificElement ? specificElement.closest("table") : null;
+
+let grade = []; // ex. A+,B etc
+const score = []; // 이수 학점들
+
+//성적 가져와서 배열에 추가하고 성적변환
+for (var i = 0; i < 7; i++) {
+  grade.push(
+    table.querySelectorAll("tbody > tr > td:nth-child(6)")[i].textContent
+  );
+}
+const tt = getScores(grade);
+
+//이수학점 가져오기
+for (var i = 0; i < 7; i++) {
+  score.push(
+    table.querySelectorAll("tbody > tr > td:nth-child(5)")[i].textContent
+  );
+}
+
+temp = 0;
+totalCredit = 0; // 이수학점 총합을 저장할 변수
+
+// for (var i = 0; i < score.length; i++) {
+//   if (grade[i] !== "P " && grade[i] !== "NP ") {
+//     temp += tt[i] * Number(score[i]);
+//     console.log(temp);
+//     totalCredit += Number(score[i]); // 이수학점 총합 업데이트
+//     console.log(totalCredit);
+//   }
+// }
+for (var i = 0; i < score.length; i++) {
+  if (grade[i] === "P " || grade[i] === "NP ") continue;
+  temp += tt[i] * Number(score[i]);
+  totalCredit += Number(score[i]); // 이수학점 총합 업데이트
+}
+
+console.log(temp);
+console.log(totalCredit); // 이수학점 총합 확인
+const avg = (temp / totalCredit).toFixed(2); // 이수학점으로 평균 계산
+
+chrome.runtime.sendMessage({ type: "calculationResult", avg: avg });
